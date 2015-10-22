50e9da078763c0f0843212e9ea8977dd0fa34045
GitsAndGlamour <omierwalls@gmail.com>
2015-10-22 08:21:56 -0400

    Started Set-up of Server Connection

--- index.php
+++ index.php
@@ -1,15 +1,26 @@
 <?php
-
+/*******************************************************************************
+    Require Autoloaders
+*******************************************************************************/
  /* Requite Composer Autoloader */
-2 require 'vendor/autoload.php';
-4
+ require 'vendor/autoload.php';
+
+ /* Require Database Class */
+ require 'config/database/database.php';
+
+ /*******************************************************************************
+     Set-up Defaults
+ *******************************************************************************/
  /* Set Default Timezone */
  date_default_timezone_set('America/New_York');
 
+/*******************************************************************************
+    Slim Framework Set-up
+*******************************************************************************/
  /* Instantiation of Slim Class */
  $application = new \Slim\Slim();
 
-/* Temporarily Route HTTP requests to echo statements using Slim get() function */
+/* Temporarily Route HTTP requests to echo statements using Slim get function */
  function addr_router($address, $application) {
    $application->get("'/" . $address . "'", function() {
      echo "Hello, this is the " . $address . " page.";
@@ -18,5 +29,19 @@
  $this->addr_router(null, $application);
  $this->addr_router('test', $application);
 
+/* Run Slim Framework */
  $application->run();
+
+/*******************************************************************************
+     Database Connection Set-up
+*******************************************************************************/
+ /* Instantiation of Database Class */
+ $db = new db('root', 'root', 'main');
+
+ /* Connect to Server */
+ $connection = $db->db_connect();
+
+/* Test Connection to Server */
+ $test_connection = $db->db_connect_fail($connection);
+
  ?>
